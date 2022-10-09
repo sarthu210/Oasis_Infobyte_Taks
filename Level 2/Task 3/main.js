d7987bcc41e72d3b8dfe1fa51317cb0e5f9c6365
Sarthak Nande <94531691+sarthu210@users.noreply.github.com>
2022-10-09 11:48:13 +0530

    Update main.js

--- Level 2/Task 3/main.js
+++ Level 2/Task 3/main.js
@@ -3,18 +3,13 @@ function add_item() {
     let item = document.getElementById("box");
     let list_item = document.getElementById("list_item");
     if(item.value != ""){
-    
         let make_li = document.createElement("LI");
         make_li.appendChild(document.createTextNode(item.value));
-    
         list_item.appendChild(make_li);
-    
         item.value=""
-        
         make_li.onclick = function(){
             this.parentNode.removeChild(this);
         }
-    
     }
     else{
     
